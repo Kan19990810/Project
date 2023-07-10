b9f2baeb4249da99b5dc5fbfa7b56c8feaf9aec9
Kan19990810 <xkk19990810@gmail.com>
2023-07-10 08:31:46 +0800

    common

--- MFA_Conformer/scripts/build_datalist.py
+++ MFA_Conformer/scripts/build_datalist.py
@@ -7,11 +7,6 @@ import pandas as pd
 
 def findAllSeqs(dirName, extension, speaker_level):
     """
-    :param dirName:
-    :param extension:
-    :param speaker_level:
-    :return:
-
     outSequences:
     Lists all the sequences with the given extension in the dirName directory
     the example of outSequences: outSequences[0]=(0, '/public/home/xiaok/xiaok/data/voxceleb2/id03145/5DdGz9cnGw8/00010.wav')
@@ -20,24 +15,32 @@ def findAllSeqs(dirName, extension, speaker_level):
     The speaker labels at the speaker_level
     the example of outSpeakers: outSpeakers[0] = id03145
     """
+
+    # os.sep: linux /; windows \\
     if dirName[-1] != os.sep:
         dirName += os.sep
     prefixSize = len(dirName)
     speakersTarget = {}
     outSequences = []
+    print("finding {}, Waiting...".format(extension))
     for root, dirs, filenames in tqdm.tqdm(os.walk(dirName, followlinks=True)):
         filtered_files = [f for f in filenames if f.endswith(extension)]
         if len(filtered_files) > 0:
+            # speakerStr: id03145
             speakerStr = os.sep.join(root[prefixSize:].split(os.sep)[:speaker_level])
             if speakerStr not in speakersTarget:
+                # 给speakerStr 一个编号 speakersTarget[id03145] = 0
                 speakersTarget[speakerStr] = len(speakersTarget)
             speaker = speakersTarget[speakerStr]
             for filename in filtered_files:
+                # full_path /public/home/xiaok/xiaok/data/voxceleb2/id03145/5DdGz9cnGw8/00010.wav
                 full_path = os.path.join(root, filename)
+                # outSequences (0, /public/home/xiaok/xiaok/data/voxceleb2/id03145/5DdGz9cnGw8/00010.wav)
                 outSequences.append((speaker, full_path))
     outSpeakers = [None for x in speakersTarget]
 
     for key, index in speakersTarget.items():
+        # 将speakersTarget 中的key, index对换： outSpeakers[0] = 'id03145'
         outSpeakers[index] = key
 
     print('find {} speakers'.format(len(outSpeakers)))
@@ -61,20 +64,23 @@ if __name__ == '__main__':
     args = parser.parse_args()
     outSequences, outSpeakers = findAllSeqs(dirName=args.dataset_dir, extension=args.extension, speaker_level=1)
     outSequences = np.array(outSequences, dtype=str)
+    # utt_spk_int_labels 说话人的编号的列表 和/public/home/xiaok/xiaok/data/voxceleb2/id03145/5DdGz9cnGw8/00010.wav同一个说话人 id03145 -> 0
     utt_spk_int_labels = outSequences.T[0].astype(int)
-    utt_pahts = outSequences.T[1]
+    # utt_paths 说话音频的路径的列表 [[/public/home/xiaok/xiaok/data/voxceleb2/id03145/5DdGz9cnGw8/00010.wav], ...]
+    utt_paths = outSequences.T[1]
     utt_spk_str_labels = []
     for i in utt_spk_int_labels:
+        # utt_spk_str_labels 'id03145'
         utt_spk_str_labels.append(outSpeakers[i])
 
-    csv_dict = {'speaker_name': utt_spk_str_labels, 'utt_paths': utt_pahts, 'utt_spk_int_labels': utt_spk_int_labels}
+    csv_dict = {'speaker_name': utt_spk_str_labels, 'utt_paths': utt_paths, 'utt_spk_int_labels': utt_spk_int_labels}
     df = pd.DataFrame(data=csv_dict)
 
     try:
         f = open(args.data_list_path, 'w')
         n = len(utt_spk_int_labels)
         for i in range(n):
-            f.write('{} {} {}\n'.format(utt_spk_str_labels[i], utt_pahts[i], utt_spk_int_labels[i]))
+            f.write('{} {} {}\n'.format(utt_spk_str_labels[i], utt_paths[i], utt_spk_int_labels[i]))
 
         df.to_csv(args.data_csv_path)
         print('Save data csv file at {}'.format(args.data_csv_path))
